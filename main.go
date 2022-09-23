19d0458b1d4b855a513202bffd421cc3758389be
Xifo <xifo.wu@gmail.com>
2022-09-23 17:26:03 +0800

    feat: 初始化路由，以及处理 404 请求

--- main.go
+++ main.go
@@ -1,40 +1,22 @@
 package main
 
 import (
-	"net/http"
-	"strings"
+	"fmt"
+	"gen-resume/bootstrap"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// 初始化 Gin 实例
-	r := gin.New()
+	router := gin.New()
 
-	// 注册中间件
-	r.Use(gin.Logger(), gin.Recovery())
+	// 初始化路由绑定
+	bootstrap.SetupRoute(router)
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"ping": "pong",
-		})
-	})
-
-	// 处理 404 请求
-	r.NoRoute(func(c *gin.Context) {
-		// 获取标头信息的 Accept 信息
-		acceptString := c.Request.Header.Get("Accept")
-		if strings.Contains(acceptString, "text/html") {
-			// 如果是 HTML 的话
-			c.String(http.StatusNotFound, "页面返回 404")
-		} else {
-			// 默认返回 JSON
-			c.JSON(http.StatusNotFound, gin.H{
-				"error_code":    404,
-				"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
-			})
-		}
-	})
-
-	r.Run(":2027")
+	err := router.Run(":2027")
+	if err != nil {
+		// 错误处理。端口被占用或其他错误
+		fmt.Println(err.Error())
+	}
 }
