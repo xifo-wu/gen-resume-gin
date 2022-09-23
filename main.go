41fb522a4acc2e303682130e3dae6c08271958f2
Xifo <xifo.wu@gmail.com>
2022-09-23 17:12:29 +0800

    feat: 集成 Gin 框架

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// 初始化 Gin 实例
+	r := gin.New()
+
+	// 注册中间件
+	r.Use(gin.Logger(), gin.Recovery())
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"ping": "pong",
+		})
+	})
+
+	// 处理 404 请求
+	r.NoRoute(func(c *gin.Context) {
+		// 获取标头信息的 Accept 信息
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			// 如果是 HTML 的话
+			c.String(http.StatusNotFound, "页面返回 404")
+		} else {
+			// 默认返回 JSON
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
+			})
+		}
+	})
+
+	r.Run(":2027")
+}
