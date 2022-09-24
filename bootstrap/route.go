60f948a465901767effe408c3edf12a516ee1316
Xifo <xifo.wu@gmail.com>
2022-09-24 15:51:17 +0800

    feat: 新增接口反问日志

--- bootstrap/route.go
+++ bootstrap/route.go
@@ -1,6 +1,7 @@
 package bootstrap
 
 import (
+	"gen-resume/app/middlewares"
 	"gen-resume/routes"
 	"net/http"
 	"strings"
@@ -23,7 +24,7 @@ func SetupRoute(router *gin.Engine) {
 
 func registerGlobalMiddleWare(router *gin.Engine) {
 	router.Use(
-		gin.Logger(),
+		middlewares.Logger(),
 		gin.Recovery(),
 	)
 }
