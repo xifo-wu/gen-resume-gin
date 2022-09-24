9d0565ae0166dd2c98eb1ff626b7cbc6cedb3f6a
Xifo <xifo.wu@gmail.com>
2022-09-24 11:00:00 +0800

    feat: 新增配置方案

--- main.go
+++ main.go
@@ -1,20 +1,34 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"gen-resume/bootstrap"
+	bootstrapConfig "gen-resume/config"
+	"gen-resume/pkg/config"
 
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	// 加载 config 目录下的配置信息
+	bootstrapConfig.Initialize()
+}
+
 func main() {
+	// 配置初始化，依赖命令行 --env 参数
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
+
 	// 初始化 Gin 实例
 	router := gin.New()
 
 	// 初始化路由绑定
 	bootstrap.SetupRoute(router)
 
-	err := router.Run(":2027")
+	err := router.Run(":" + config.Get("app.port"))
 	if err != nil {
 		// 错误处理。端口被占用或其他错误
 		fmt.Println(err.Error())
