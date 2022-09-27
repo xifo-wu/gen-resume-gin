202862fe2c0b46e5efcedd7259c6674f8cbb17a4
Xifo <xifo.wu@gmail.com>
2022-09-27 15:00:59 +0800

    feat: 新增分页请求验证器

--- app/controllers/api/v1/manager/users_controller.go
+++ app/controllers/api/v1/manager/users_controller.go
@@ -3,6 +3,7 @@ package manager
 import (
 	v1 "gen-resume/app/controllers/api/v1"
 	"gen-resume/app/models/user"
+	"gen-resume/app/requests"
 	"gen-resume/pkg/response"
 
 	"github.com/gin-gonic/gin"
@@ -14,6 +15,11 @@ type UsersController struct {
 
 // Index 所有用户
 func (ctrl *UsersController) Index(c *gin.Context) {
+	request := requests.PaginationRequest{}
+	if ok := requests.Validate(c, &request, requests.Pagination); !ok {
+		return
+	}
+
 	data, pager := user.Paginate(c, 10)
 	response.JSON(c, gin.H{
 		"data": data,
